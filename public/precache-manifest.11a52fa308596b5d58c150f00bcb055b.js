742c249179dee753b1ca33960fe8694ec28838ba
Ricardo Torres <ricardots211@gmail.com>
2019-09-26 15:35:08 -0500

    deploy to firebase

---
+++ public/precache-manifest.11a52fa308596b5d58c150f00bcb055b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508f485daf1d22caf5626c70ae67abcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5cf02b18270ee3eb80",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0917ed7770af73836cb1",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "ec5cf02b18270ee3eb80",
+    "url": "/static/js/2.ec141478.chunk.js"
+  },
+  {
+    "revision": "0917ed7770af73836cb1",
+    "url": "/static/js/main.da47c5ee.chunk.js"
+  },
+  {
+    "revision": "4e52d0a9b3dbebdad20a",
+    "url": "/static/js/runtime~main.3854d27a.js"
+  }
+]);
